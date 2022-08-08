2548a3de98991fa8820ce00e4016a6342d4762f3
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-08-08 14:31:57 +0100

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import RocketContainer from './components/Rockets/RocketContainer';
+import MissionsContainer from './components/Missions/MissionsContainer';
+import Profile from './components/Profile/myProfile';
 
 function App() {
   return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<RocketContainer />} />
+        <Route path="/missions" element={<MissionsContainer />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Rockets/RocketContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function RocketContainer() {
+  return (
+    <div>RocketContainer</div>
+  );
+}
